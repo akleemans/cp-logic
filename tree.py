a50100b239efc9eb6313abebf14a190cfce0f978
Adrianus Kleemans <a.kleemans@gmail.com>
2014-04-19 14:13:07 +0200

    Implemented scrolling in dchains()-tree.

--- tree.py
+++ tree.py
@@ -26,6 +26,7 @@ RED = (200, 0, 0)
 GREEN = (0, 200, 0)
 BLUE = (0, 0, 200)
 GRAY = (192, 192, 192)
+WHITE = (255,255,255)
 
 class Tree(object):
     '''
@@ -37,11 +38,13 @@ class Tree(object):
         Constructor
         '''
         self.start_node = start_node
-        self.size = (1200, 800)
+        self.size = (1200, 1000)
         self.nodes = []
 
         # calculate depth
         self.depth = self.get_depth()
+        self.image_size = (max((2**self.depth)*50, self.size[0]), 1000)
+        #print self.depth
 
         # assign coordinates to nodes
         self.nodes = self.get_nodes()
@@ -49,8 +52,18 @@ class Tree(object):
 
         # preparing canvas
         pygame.init()
-        self.disp = pygame.display.set_mode([self.size[0], self.size[1]], RESIZABLE)
         pygame.display.set_caption("dchains-tree")
+        self.disp = pygame.display.set_mode([self.size[0], self.size[1]], RESIZABLE)
+        
+        self.dispRect = self.disp.get_rect()
+        self.world = pygame.Surface((self.image_size[0], self.image_size[1]))
+        
+        self.ratio = (1.0 * self.dispRect.width) / self.world.get_width()
+        scrollThick = 20
+        self.track = pygame.Rect(self.dispRect.left, self.dispRect.bottom - scrollThick, self.dispRect.width, scrollThick)   
+        self.knob = pygame.Rect(self.track)
+        self.knob.width = self.track.width * self.ratio
+        self.scrolling = False
 
         while not self.catch_event():
             self.draw_board()
@@ -62,7 +75,7 @@ class Tree(object):
         return os.path.join(relative)
 
     def set_coordinates(self):
-        y_step = self.size[1]/(self.depth+1)
+        y_step = self.image_size[1]/(self.depth+1)
         y = 0
 
         # assign coordinates
@@ -71,10 +84,10 @@ class Tree(object):
             y = y_step * depth_of_node
 
             if node.parent == None:
-                x = self.size[0]/2
+                x = self.image_size[0]/2
             else:
                 x = node.parent.x
-                x_step = (self.size[0]/(2**(len(node.name)-1))) / 2
+                x_step = (self.image_size[0]/(2**(len(node.name)-1))) / 2
                 if node.name[-1] == '0': x -= x_step
                 elif node.name[-1]  == '1': x += x_step
 
@@ -115,7 +128,8 @@ class Tree(object):
         '''
         Starts up a pygame-canvas and draws the graph.
         '''
-        pygame.draw.rect(self.disp, (254, 254, 254), (0, 0, self.size[0], self.size[1]))
+        self.world.fill(WHITE)
+        #pygame.draw.rect(self.world, (254, 254, 254), (0, 0, self.image_size[0], self.image_size[1]))
         
         #small_font = pygame.font.SysFont(u'dejavuserif', 14)
         #normal_font = pygame.font.SysFont(u'monospace', 16)
@@ -133,12 +147,12 @@ class Tree(object):
 
             # draw line to parent
             if node.parent != None:
-                pygame.draw.line(self.disp, GRAY, (node.parent.x, node.parent.y), (node.x, node.y), 1)
+                pygame.draw.line(self.world, GRAY, (node.parent.x, node.parent.y), (node.x, node.y), 1)
                 # draw edge label
-                self.disp.blit(edge_label, ((node.parent.x + node.x)/2, (node.parent.y + node.y)/2))
+                self.world.blit(edge_label, ((node.parent.x + node.x)/2, (node.parent.y + node.y)/2))
 
             # draw label
-            self.disp.blit(label, (node.x-x_offset, node.y-y_offset))
+            self.world.blit(label, (node.x-x_offset, node.y-y_offset))
 
             # draw if it's an axiom or not
             circle_color = BLACK
@@ -152,15 +166,19 @@ class Tree(object):
                 circle_color = BLACK
 
             # draw node shape
-            pygame.draw.rect(self.disp, circle_color, (node.x-x_offset-5, node.y-y_offset-2, x_offset*2+10, y_offset*2+4), 2)
+            pygame.draw.rect(self.world, circle_color, (node.x-x_offset-5, node.y-y_offset-2, x_offset*2+10, y_offset*2+4), 2)
             #pygame.draw.circle(self.disp, circle_color, (node.x+5, node.y+7), 20, 2)
 
             # draw if node is currently active
             if node.status == 'marked':
                 label = normal_font.render(node.chain, 1, BLACK)
                 #print 'Size of label:', label.get_width()
-                self.disp.blit(label, (node.x-label.get_width()/2+5, node.y-40))
+                self.world.blit(label, (node.x-label.get_width()/2+5, node.y-40))
 
+        self.disp.blit(self.world, ((self.knob.left / self.ratio) * -1 , 0))
+        pygame.draw.rect(self.disp, WHITE, self.track, 0 )
+        pygame.draw.rect(self.disp, BLUE, self.knob.inflate(0,-5), 2)
+            
         pygame.display.flip()
 
     def catch_event(self):
@@ -179,6 +197,20 @@ class Tree(object):
                 self.size = event.dict['size']
                 self.disp = pygame.display.set_mode([self.size[0], self.size[1]], RESIZABLE)
                 self.set_coordinates()
+
+            elif (event.type == pygame.MOUSEMOTION and self.scrolling):
+                if event.rel[0] != 0:
+                    move = max(event.rel[0], self.track.left - self.knob.left)
+                    move = min(move, self.track.right - self.knob.right)
+
+                    if move != 0:
+                        self.knob.move_ip((move, 0))
+                            
+            elif event.type == pygame.MOUSEBUTTONDOWN and self.knob.collidepoint(event.pos):
+                self.scrolling = True
+                    
+            elif event.type == pygame.MOUSEBUTTONUP:
+                self.scrolling = False
 
     def handle_click(self, pos):
         '''
